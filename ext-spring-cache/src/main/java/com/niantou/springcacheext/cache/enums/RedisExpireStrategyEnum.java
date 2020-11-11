3332797d20eb5297c7ecfb0dd1eb50c93c81b70b
JustryDeng <dengeryanger@gmail.com>
2020-11-11 18:46:31 +0800

    ext-spring-cache

--- ext-spring-cache/src/main/java/com/niantou/springcacheext/cache/enums/RedisExpireStrategyEnum.java
+++ ext-spring-cache/src/main/java/com/niantou/springcacheext/cache/enums/RedisExpireStrategyEnum.java
@@ -13,6 +13,8 @@ public enum RedisExpireStrategyEnum {
     
     /**
      * 若配置文件中配置有过期时间，那么走配置文件中配置的的过期时间；若配置文件中没有配置过期时间，那么走注解中指定的过期时间
+     * <p>
+     * 注:过期时间必须与cacheName一起配置才会生效。即:只配置了过期时间，但是没有指定任何cacheName(或指定的cacheName不对)时，也是不会走配置的过期时间的。
      */
     AUTO,
     
